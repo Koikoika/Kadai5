5fc8664d4f0bfda21856cbb71642a6c0323657a0
Karin.T <Karin.T@172.19.12.64>
2018-05-17 14:51:58 +0900

    translateEngメソッドを途中まで制作しました

--- src/intToEng/IntToEng.java
+++ src/intToEng/IntToEng.java
@@ -24,9 +24,22 @@ public class IntToEng {
 			"sixteen",
 			"seventeen",
 			"eighteen",
-			"nineteen",
+			"nineteen"
 	};
 
+	static final String[] second = {
+		"",
+		"",
+		"twenty",
+		"thirty",
+		"fourty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety"
+	};
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
@@ -36,7 +49,18 @@ public class IntToEng {
 	}
 	
 	static String translateEng(int n) {
-        return "";
+        String[] number = {};
+		int a,b;
+		for(int i=n; i>0; i = i/100) {
+		a = i%10;
+		b = a%10;
+		if(b<2) number[0] = first[b*10+a];
+		else {
+			number[0] = first[a];
+			number[1] = second[b];
+		 }
+		}
+		return "";
     }
 
 }
